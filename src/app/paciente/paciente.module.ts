f528fe91c5ffa5d8b489de2ba22f36ad4777abae
fofoMedrano23 <rod_medrano@hotmail.com>
2018-08-22 12:36:16 -0600

    added create component

--- src/app/paciente/paciente.module.ts
+++ src/app/paciente/paciente.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule,Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import {
   FormioResource,
   FormioResourceConfig,
@@ -9,20 +9,24 @@ import {
 } from 'angular-formio/resource';
 import { PacienteResourceComponent } from './paciente-resource/paciente-resource.component';
 import { PacienteViewComponent } from './paciente-view/paciente-view.component';
+import { PacienteCreateComponent } from './paciente-create/paciente-create.component';
+import { FormioModule } from 'angular-formio';
 
 /*inject the participant routes*/
 const pacienteResourceRoutes: Routes = FormioResourceRoutes({
   view: PacienteViewComponent,
-  resource: PacienteResourceComponent
+  resource: PacienteResourceComponent,
+  create: PacienteCreateComponent
 });
 
 @NgModule({
   imports: [
     CommonModule,
     FormioResource,
-    RouterModule.forChild(pacienteResourceRoutes)
+    RouterModule.forChild(pacienteResourceRoutes),
+    FormioModule
   ],
-  declarations: [PacienteResourceComponent, PacienteViewComponent],
+  declarations: [PacienteResourceComponent, PacienteViewComponent, PacienteCreateComponent],
   providers: [
     FormioResourceService,
     {provide: FormioResourceConfig, useValue: {
